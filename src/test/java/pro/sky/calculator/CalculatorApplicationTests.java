34d3c3bde247911cd78d4205526d6ae752dc4d9b
Home <yulaguby@gmail.com>
2022-02-08 19:16:48 +0300

    done

--- src/test/java/pro/sky/calculator/CalculatorApplicationTests.java
+++ src/test/java/pro/sky/calculator/CalculatorApplicationTests.java
@@ -2,12 +2,35 @@ package pro.sky.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pro.sky.calculator.Controller.CalculatorController;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void calculatorTest() {
+		assertEquals("error in add()", 3,  calculatorService(3, 0));
+		assertEquals("error in add()", 2, calculatorService(1, 1));
+		assertEquals("error in add()", 7, calculatorService(6, 1));
+	}
+
+    private Object calculatorService(int i, int i1) {
+        return null;
+    }
+
+    public void calculatorAddFail() {
+		assertEquals("error in add()", 0,  calculatorService(4, 2));
+	}
+
+	public void calculatorSubPass() {
+		assertEquals("error in sub()", -1,  calculatorService(3, 0));
+		assertEquals("error in sub()", 2,  calculatorService(1, 1));
+		assertEquals("error in sub()", 4,  calculatorService(2, 1));
+	}
+	public void calculatorSubFail() {
+		assertEquals("error in add()", 0, calculatorService(4, 2));
 	}
 
 }
